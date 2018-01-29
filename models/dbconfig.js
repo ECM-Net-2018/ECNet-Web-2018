16a36234ff68694630ca8e308363092f42e96705
root <chugare@126.com>
2018-01-29 11:04:20 +0800

    first commit

---
+++ models/dbconfig.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zgw on 2017/3/20.
+ */
+let dbconfig = {
+    database: 'ecm', // 使用哪个数据库
+    username: 'root', // 用户名
+    password: 'root', // 口令
+    host: 'localhost', // 主机名
+    port: 3306, // 端口号，MySQL默认3306
+    dialect:'mysql',
+};
+
+module.exports = dbconfig;
